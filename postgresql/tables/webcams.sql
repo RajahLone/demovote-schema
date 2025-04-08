d87721e84fad0c4c49fb17f44cca387974b09751
rajah <pierre.tonthat@free.fr>
2025-04-08 12:42:38 +0200

    dev en cours

--- postgresql/tables/webcams.sql
+++ postgresql/tables/webcams.sql
@@ -1,8 +1,19 @@
 CREATE TABLE IF NOT EXISTS vote.webcams
 (
+  date_modification timestamp without time zone,
   id integer NOT NULL PRIMARY KEY,
   crc32 bigint NOT NULL,
   vue bytea NOT NULL
 )
 TABLESPACE vote;
 ALTER TABLE IF EXISTS vote.webcams OWNER to vote;
+
+CREATE FUNCTION vote.dateModificationWebcam() RETURNS TRIGGER AS $$
+BEGIN
+  NEW.date_modification = now();
+  return NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+ALTER FUNCTION vote.dateModificationWebcam() OWNER TO vote;
+
+CREATE OR REPLACE TRIGGER dateModificationWebcam BEFORE UPDATE ON vote.webcams FOR EACH ROW EXECUTE FUNCTION vote.dateModificationWebcam();
